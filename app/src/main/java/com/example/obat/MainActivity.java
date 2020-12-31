45171100b387f92509dde65cc8aeb626539d22cd
Restaadit <restaadit10@gmail.com>
2020-12-31 18:23:44 +0700

    karepeakurah

--- app/src/main/java/com/example/obat/MainActivity.java
+++ app/src/main/java/com/example/obat/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.obat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void Pindah1(View view) {
+        Intent intent = new Intent(MainActivity.this, TambahData.class);
+        startActivity(intent);
+    }
+    public void Pindah2(View view) {
+        Intent intent = new Intent(MainActivity.this, LihatData.class);
+        startActivity(intent);
     }
 }
